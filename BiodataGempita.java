3d07548f67b11e009a01ce4fe1e5739dbb76a3e1
gempitadini <gempitadini05@gmail.com>
2024-09-06 13:34:49 +0700

    FGT 1

---
+++ BiodataGempita.java
@@ -0,0 +1,9 @@
+public class BiodataGempita {
+    
+}public static void main(String[] args) {
+    System.out.println(Nama                 : Gempita Fitri Nurdini);
+    System.out.println(Tempat Tanggal Lahir : Kediri, 27 Mei 2006);
+    System.out.println(Alamat               : Jl. Rambutan Purwoasri Kediri);
+    System.out.println(Jenis kelamin        : Perempuan);
+    System.out.println(NIM                  : 244107060083);
+}
